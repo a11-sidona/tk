70725e3b5031ad2d3ea0681e493ee085096315f3
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-11-23 16:24:54 +0700

    tes

--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.http import response
+from django.shortcuts import redirect, render
 
 
 def home(request):
+    if request.method == "POST":
+        nama = request.POST["nama"]
+
+        response = {}
+        response["nama"] = nama
+        return render(request, "main/next.html", response)
+
     return render(request, "main/home.html")
+
+
+def sukses(request):
+    if request.method == 'POST':
+        response = {}
+        response['nama'] = request.POST['nama']
+        response['alamat'] = request.POST['alamat']
+        return render(request, "main/sukses.html", response)
+
+    return redirect('/')
