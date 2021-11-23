7ab0feaac058b3ab3a066f4b89ba7a764e3d9954
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-11-23 14:21:04 +0700

    add skeleton

--- main/views.py
+++ main/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, "main/home.html")
+
+
+def penggalangan_dana(request):
+    return render(request, "main/penggalangan.html")
