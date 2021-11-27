66bf7b0846d69f42f92fa19c9c51ff04139035cb
Muhammad Rizqi Rizaldi <muhammad.rizqi03@ui.ac.id>
2021-11-27 21:17:58 +0700

    Create Page Login and Register

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,11 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("sukses", views.sukses)
+    path("sukses", views.sukses),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("register-admin", views.register_admin, name="register-admin"),
+    path("register-user", views.register_user, name="register-user"),
+    path("register-user-individu", views.register_user_individu, name="register-user-individu"),
+    path("register-user-organisasi", views.register_user_organisasi, name="register-user-organisasi")
 ]
